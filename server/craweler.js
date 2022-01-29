02295ef3eddfddecebdfccfab237fdf8a83d9c4d
dmsdl950823 <dmsdl950823@gmail.com>
2022-01-29 19:00:12 +0900

    Updated #220129 크롤러 완성 및 타이틀 겟

--- server/craweler.js
+++ server/craweler.js
@@ -56,51 +56,96 @@ const fetchShelves = async () => {
 // 사용 예시
 // https://zenscrape.com/how-to-scrape-amazon-product-information-with-nodejs-and-puppeteer/
 
-const forPuppeteerTest = () => {
+const forPuppeteerTest = async () => {
 
   console.log('Browser Start!')
   // console.log('pupeteer', puppeteer)
   
-  puppeteer.launch({
+  const browser = await puppeteer.launch({
     headless: false,
     // https://free-proxy-list.net/ (프록시서버)
     // args: ['--proxy-server=47.243.135.104:8080', '--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080','--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'],
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080','--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'],
     slowMo: 30,   
-  }).then(async browser => {
-
-    const page = await browser.newPage() // 크롬 브라우저의 탭 하나 생성
-    
-    await page.setViewport({ width: 1920, height: 1080 })
-    // await page.screenshot({ path: 'screenshot/open.png' })
-    // await page.goto("https://www.amazon.com/s?k=supplies&page=2&qid=1641879572&ref=sr_pg_2");
-
-    console.log('페이지 로딩 된건디?')
-    
-    Promise.all([
-      await page.goto("https://www.naver.com/"),
-      await page.waitForSelector() // 화면이 로딩될때까지 기다려라
-    ])
-
-    // https://www.youtube.com/watch?v=eynT_cMvOTU 동영상 강의
-    console.log('finish waiting..!!!')
-
-    let target = "//span[text()='쇼핑']/ancestor::a"
-    await page.waitForXPath(target) // 해당 xpath 를 찾을때 까지 기본적으로 30 초 기다림
-    let s = await page.$x(target)
-    s = s[0]
-    
-    await Promise.all([
-      await s.click(),
-      await page.waitForSelector() // 화면이 로딩될때까지 기다려라
-    ])
-
-    await page.waitForTimeout(3000) // 3 초 정도 기다림
-    await browser.close()
-  
-  }).catch(function(error) {
-      console.error(error)
   })
+
+  const page = await browser.newPage() // 크롬 브라우저의 탭 하나 생성
+  
+  // https://www.tabnine.com/code/javascript/functions/puppeteer/Page/waitForSelector :: 참고 문헌
+  await page.setViewport({ width: 1920, height: 1080 })
+  // await page.goto("https://www.amazon.com/s?k=supplies&page=2&qid=1641879572&ref=sr_pg_2");
+  // await page.goto("https://www.naver.com/")
+  // await page.screenshot({ path: 'screenshot/open.png' })
+
+  // await page.click('.list_nav.type_fix > .nav_item:nth-of-type(4)')
+  // await page.waitForSelector()
+
+  // console.log('bye!!!!')
+  // await browser.close() 
+
+  await page.goto('https://www.amazon.com')
+
+  // postcode 설정
+  // await page.click('#nav-global-location-slot')
+  // await page.waitForSelector('#GLUXSignInButton')
+  // await page.type('.GLUX_Full_Width.a-declarative', '10001')
+  // await page.click('#GLUXZipUpdate-announce')
+
+  // await page.waitForSelector('.a-popover-footer')
+  // await page.click('.a-popover-footer #GLUXConfirmClose')
+
+  // console.log('클릭한겨?')
+
+  // 텍스트 입력, 페이지 끝까지 기다리기
+  await page.type('#twotabsearchtextbox', 'cat')
+  await page.click('#nav-search-submit-button')
+  await page.waitForSelector('.navFooterBackToTopText')
+
+  // 목록 가져올때까지 기다리기
+  await page.waitForSelector('.s-main-slot')
+  const elements = await page.$$('.s-main-slot > .sg-col-4-of-12')
+
+  for (let i = 0; i < elements.length; i++) {
+    const item = elements[i]
+
+    const title = await item.$eval('h2 a.a-link-normal.s-underline-text span.a-text-normal', el => el.textContent)
+    console.log(title)
+  }
+
+  // ===
+  // ===
+  // ===
+  // await page.screenshot({ path: 'amazon_nyan_cat_pullovers_list.png' })
+  // await page.click('#pagnNextString')
+  // await page.waitForSelector('#resultsCol')
+  // const pullovers = await page.$$('a.a-link-normal.a-text-normal')
+  // await pullovers[2].click()
+  // await page.waitForSelector('#ppd')
+  // await page.screenshot({ path: screenshot })
+  
+  console.log('bye!!')
+  await browser.close()
+  // console.log('See screenshot: ' + screenshot)
+
+  //   // https://www.youtube.com/watch?v=eynT_cMvOTU 동영상 강의
+  //   // console.log('finish waiting..!!!')
+
+  //   // let target = "//span[text()='쇼핑']/ancestor::a"
+  //   // await page.waitForXPath(target) // 해당 xpath 를 찾을때 까지 기본적으로 30 초 기다림
+  //   // let s = await page.$x(target)
+  //   // s = s[0]
+    
+  //   // await Promise.all([
+  //   //   await s.click(),
+  //   //   await page.waitForSelector() // 화면이 로딩될때까지 기다려라
+  //   // ])
+
+  //   // await page.waitForTimeout(3000) // 3 초 정도 기다림
+  //   // await browser.close()
+  
+  // }).catch(function(error) {
+  //     console.error(error)
+  // })
 }
 
 forPuppeteerTest()
