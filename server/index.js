8370e33722dc00a69400fa2dfbb405b72de18aa5
jejung <jejung@growthsoft.co.kr>
2022-01-29 23:05:48 +0900

    Updated #220129 크롤러 DB 구축중

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require('express')
-const bodyParser = require('bodyParser')
 const cors = require('cors')
+const mongoose = require('mongoose') // mongodb 연결
 
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 8000;
 
 // Middleware
+app.use(cors());
+app.use(express.json());
 
-app.use(bodyParser.json())
-app.use(cors)
+const uri = process.env.ATLAS_URI // db URI
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect(uri)
+const connection = mongoose.connection
+connection.once('open', () => {
+  console.log('MongoDB database connection establised successfully')
+})
+
+const excerciseRouter = require('./routes/exercies')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', excerciseRouter)
+app.use('/users', usersRouter)
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
+// app.listen(port, () => )
