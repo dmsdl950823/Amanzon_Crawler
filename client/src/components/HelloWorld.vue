f3133fca05771518503fd1286a9173cf0e0d5f6d
dmsdl950823 <dmsdl950823@gmail.com>
2022-02-08 21:45:28 +0900

    Updated #220208 크롤러 가공용 UI 생성

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="hello">
+    <va-file-upload
+      v-model="basic"
+      @input="upload"
+    />
+
     <va-button
       color="info"
       gradient
@@ -7,59 +12,84 @@
     >
       SAVE
     </va-button>
+
     <va-popover
       message="Popover text"
     >
       <va-button>SAVE</va-button>
     </va-popover>
 
-    <va-data-table
-      :items="items"
-      :columns="columns"
-      striped
-    >
-      <template #cell(actions)>
-        <va-button
-          flat
-          icon="delete"
-        />
-      </template>
-    </va-data-table>
+    <WijmoChart />
   </div>
 </template>
 
 <script>
+import WijmoChart from './WijmoChart'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    WijmoChart
+  },
   data () {
     return {
-      items: [
-        {
-          name: 'Leanne Graham',
-          username: 'Bret',
-          email: 'Sincere@april.biz'
-        },
-        {
-          name: 'Ervin Howell',
-          username: 'Antonette',
-          email: 'Shanna@melissa.tv'
-        },
-        {
-          name: 'Clementine Bauch',
-          username: 'Samantha',
-          email: 'Nathan@yesenia.net'
-        },
-        {
-          name: 'Patricia Lebsack',
-          username: 'Karianne',
-          email: 'Julianne.OConner@kory.org'
-        }
-      ],
+      basic: [],
+      items: [],
       columns: [
-        { key: 'name', sortable: true },
-        { key: 'username', sortable: true },
-        { key: 'email', sortable: true },
+        { key: 'title', sortable: false, width: 100 },
+        // { key: 'image', sortable: false },
+        { key: 'price', sortable: false },
+        { key: 'href', sortable: false },
+        { key: 'asin', sortable: false },
+        { key: 'rating', sortable: false },
+        { key: 'brand', sortable: false },
+        { key: 'item_weight', sortable: false },
+        { key: 'size', sortable: false },
+        { key: 'customer_reviews', sortable: false },
+        { key: 'best_sellers_rank', sortable: false },
+        { key: 'BSR', sortable: false },
         { key: 'actions', width: 80 }
+      ]
+    }
+  },
+
+  methods: {
+    upload (event) {
+      const file = event.target.files[0]
+
+      const reader = new FileReader()
+      reader.addEventListener('load', () => {
+        this.items = this.tsvToJson(reader.result)
+        console.log(this.items)
+      }, false)
+
+      if (file) {
+        reader.readAsText(file)
+      }
+    },
+
+    tsvToJson (tsv) {
+      const rows = tsv.split('\r\n')
+      const jsonArray = []
+      const header = rows[0].split('\t')
+
+      for (let i = 1; i < rows.length; i++) {
+        const obj = {}
+        const row = rows[i].split('\t')
+        for (let j = 0; j < header.length; j++) {
+          obj[header[j]] = row[j]
+        }
+
+        jsonArray.push(obj)
+      }
+
+      return jsonArray
+    },
+
+    deleteItemById (id) {
+      this.items = [
+        ...this.items.slice(0, id),
+        ...this.items.slice(id + 1)
       ]
     }
   }
