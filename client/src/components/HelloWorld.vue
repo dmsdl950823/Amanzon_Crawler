d2b300579272f1e98d0c4adf22e40e2a0923885c
dmsdl950823 <dmsdl950823@gmail.com>
2022-02-08 20:16:35 +0900

    Updated #220208 client 추가중

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <va-button
+      color="info"
+      gradient
+      class="mr-4"
+    >
+      SAVE
+    </va-button>
+    <va-popover
+      message="Popover text"
+    >
+      <va-button>SAVE</va-button>
+    </va-popover>
+
+    <va-data-table
+      :items="items"
+      :columns="columns"
+      striped
+    >
+      <template #cell(actions)>
+        <va-button
+          flat
+          icon="delete"
+        />
+      </template>
+    </va-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      items: [
+        {
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz'
+        },
+        {
+          name: 'Ervin Howell',
+          username: 'Antonette',
+          email: 'Shanna@melissa.tv'
+        },
+        {
+          name: 'Clementine Bauch',
+          username: 'Samantha',
+          email: 'Nathan@yesenia.net'
+        },
+        {
+          name: 'Patricia Lebsack',
+          username: 'Karianne',
+          email: 'Julianne.OConner@kory.org'
+        }
+      ],
+      columns: [
+        { key: 'name', sortable: true },
+        { key: 'username', sortable: true },
+        { key: 'email', sortable: true },
+        { key: 'actions', width: 80 }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
+
+const app = createApp(App)
+
+app.use(VuesticPlugin)
+app.mount('#app')
